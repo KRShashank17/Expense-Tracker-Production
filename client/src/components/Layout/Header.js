558c60122895ca093bac169075ef765eefb7cd6d
Shashank K R <krshashank1118@gmail.com>
2024-01-21 13:07:28 +0530

    Login , Logout working

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,7 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link , useNavigate} from 'react-router-dom';
+import {message} from 'antd'
 
 const Header = () => {
+  const [loginUser , setLoginUser] = useState('');
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user)
+      setLoginUser(user);
+  },[])
+
+  const navigate = useNavigate();
+  const logoutHandler = ()=>{
+    localStorage.removeItem('user');
+    message.success('Logout Successful');
+    navigate('/login');
+  }
+  
   return (
     <>
         {/* Responsive NavBar Bootstrap */}
@@ -22,13 +38,23 @@ const Header = () => {
             <Link to="/" className="navbar-brand" >
               Expense Tracker
             </Link>
+            
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  {/* // login */}
               <li className="nav-item">
-                <Link to="/" className="nav-link active" aria-current="page">
-                  Home
-                </Link>
+                  {"  "}
+                  <p className='nav-link'> {loginUser && loginUser.name} </p>
+                  {"  "}
+              </li>
+
+                  {/* // logout */}
+              <li className="nav-item">
+                <button  className="btn btn-primary" onClick = {logoutHandler}>
+                  Logout
+                </button>
               </li>
             </ul>
+
           </div>
         </div>
       </nav>

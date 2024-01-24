3a9267fb2c2e93b2bad5d490293fe01759d22741
Shashank K R <krshashank1118@gmail.com>
2024-01-24 16:43:53 +0530

    Production ready Changes - static file in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const connectDb = require('./config/connectDB');
+const path = require('path');
 
 // dotenv config
 require('dotenv').config();
@@ -21,6 +22,14 @@ app.use(morgan('dev'));
 // routes
 app.use("/api/v1/users" , require("./routes/userRoute"));
 app.use("/api/v1/transactions" , require("./routes/transactionRoute"))
+
+// Static files - Production
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
 
 const PORT = 8000 || process.env.PORT;
 
